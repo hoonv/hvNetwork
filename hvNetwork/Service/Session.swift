005560151b561c316c6769ab117edea9322070c0
hoonv <chaeefe@gmail.com>
2020-10-23 14:15:09 +0900

    feat create session class
    - 최상위 클래스인 세션 생성

---
+++ hvNetwork/Service/Session.swift
@@ -0,0 +1,50 @@
+//
+//  HVNetwork.swift
+//  hvNetwork
+//
+//  Created by 채훈기 on 2020/10/22.
+//
+
+import Foundation
+
+public class Session {
+    
+    public static let shared = Session()
+    
+    private init() {
+        
+    }
+    
+    public func request(_ convertible: URLConvertible,
+                        method: HTTPMethod = .get,
+                        parameter: Parameters? = nil,
+                        headers: HTTPHeaders? = nil) -> DataRequest {
+        
+        return DataRequest(url: convertible,
+                           method: method,
+                           parameters: parameter,
+                           headers: headers)
+    }
+    
+    public func request<T: Decodable>(_ convertible: URLConvertible,
+                                      method: HTTPMethod = .get,
+                                      parameter: Parameters? = nil,
+                                      headers: HTTPHeaders? = nil) -> DataRequestAsDecodable<T> {
+        
+        return DataRequestAsDecodable<T>(url: convertible,
+                                      method: method,
+                                      parameters: parameter,
+                                      headers: headers)
+    }
+    
+    public func download(_ convertible: URLConvertible,
+                         method: HTTPMethod = .get,
+                         parameter: Parameters? = nil,
+                         headers: HTTPHeaders? = nil) -> DataRequest {
+        
+        return DataRequest(url: convertible,
+                           method: method,
+                           parameters: parameter,
+                           headers: headers)
+    }
+}
