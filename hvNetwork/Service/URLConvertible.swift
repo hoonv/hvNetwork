7b60db0c0fa4d16eef02174d11e8e531d1ae518c
hoonv <chaeefe@gmail.com>
2020-10-23 14:12:32 +0900

    feat: create url convertible
    - string and url is urlconvertible

---
+++ hvNetwork/Service/URLConvertible.swift
@@ -0,0 +1,28 @@
+//
+//  URLConvertible.swift
+//  hvNetwork
+//
+//  Created by 채훈기 on 2020/10/22.
+//
+
+import Foundation
+
+public protocol URLConvertible {
+    
+    func asURL() throws -> URL
+}
+
+extension String: URLConvertible {
+    
+    public func asURL() throws -> URL {
+        guard let url = URL(string: self) else { throw NetworkError.missingURL }
+        return url
+    }
+}
+
+extension URL: URLConvertible {
+    
+    public func asURL() throws -> URL {
+        return self
+    }
+}
